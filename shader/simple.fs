97d1e72868960b751b80ded27acce649612d6420
DogGuyMan <escatrgot@gmail.com>
2024-11-28 17:21:25 +0900

    [Update] : uniform으로 변수 전

--- shader/simple.fs
+++ shader/simple.fs
@@ -1,9 +1,9 @@
 #version 330 core
 
-in vec4 vertexColor; // vs에 이미 out으로 정의된 vertexColor가 입력될 변수임 ( 같은 변수명이며, 같은 타입이여야함 ) 
-out vec4 FragColor; // 최종 출력 색상
+uniform vec4 color; // uniform 코드는 외부 cpp에서 받아올 예정이다.
+out vec4 FragColor; 
 
 void main() 
 {
-    FragColor = vertexColor;
+    FragColor = color;
 }
